37b59fc0f00c3328ae266efea276f4ded973c5aa
Eldhose M Peter <eldhosempeter07@gmail.com>
2025-09-14 18:05:10 -0400

    fix: Update Vercel Json File And Vite Config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,30 @@
 import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
+import vuetify from "vite-plugin-vuetify";
 
 // Config
 export default defineConfig(({ command }) => ({
-  plugins: [vue(), command === "serve" ? vueDevTools() : null].filter(Boolean),
+  plugins: [
+    vue(),
+    vuetify({ autoImport: true }), // Add this line
+    command === "serve" ? vueDevTools() : null,
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   base: "./",
+  build: {
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vuetify: ["vuetify"],
+        },
+      },
+    },
+  },
 }));

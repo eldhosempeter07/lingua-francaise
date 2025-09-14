27f2edfc9ec358cbf0566cc800e757595e73f533
Eldhose M Peter <eldhosempeter07@gmail.com>
2025-09-14 17:17:17 -0400

    fix: Rebuild App

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 
+// Config
 export default defineConfig(({ command }) => ({
   plugins: [vue(), command === "serve" ? vueDevTools() : null].filter(Boolean),
   resolve: {
